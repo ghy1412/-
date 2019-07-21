10be6e983921640425c252ab6d8e2d55ccdc1929
Guoxmh <1094185589@qq.com>
2019-07-21 23:44:59 +0800

    thread

---
+++ ThreadJava/src/Test.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.locks.Lock;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+    }
+}
